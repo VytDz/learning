9b6b872e746911f62c4cf221920c992f606c5569
Vytenis Dzvonkas <dzvonkasvytenis@gmail.com>
2018-08-19 23:06:17 +0300

    working on fundamentals of react

---
+++ react5/src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import loaderSrc from '../../Assets/insta-stories-logo-30fps.gif'
+
+const Loader = props => (
+    <div>
+        style = {{ width: 75 }}
+        <img src="insta-stories-logo-30fps.gif" alt="loader icon"/>
+    </div>
+);
+
+export default Loader;
